aa64298e9c3a741b64f6fe7f3d1a1b6b42328713
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2024-09-26 23:35:39 +0300

    commit correction

--- monty.h
+++ monty.h
@@ -2,13 +2,14 @@
 #define MONTY_H
 
 #define GNU_SOURCE
+#define _POSIX_C_SOURCE 200809L
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <fnctl.h>
-#include <sys/type.h>
+#include <fcntl.h>
+#include <sys/types.h>
 #include <ctype.h>
 
 /**
